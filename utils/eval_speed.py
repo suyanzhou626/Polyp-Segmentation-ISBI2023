b652f8bb05807747346f0a5c85023934c807bb7b
legendchilli <37359271+suyanzhou626@users.noreply.github.com>
2022-10-06 23:43:10 +0800

    Update eval_speed.py

--- utils/eval_speed.py
+++ utils/eval_speed.py
@@ -1,25 +1,25 @@
 model = your_model() #
 device = torch.device("cuda")
-  model.to(device)
-  dummy_input = torch.randn(1, 3, 352, 352,dtype=torch.float).to(device)
-  starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
-  repetitions = 1000
-  timings=np.zeros((repetitions,1))
-  #GPU-WARM-UP
-  for _ in range(10):
-      _ = model(dummy_input)
-  # MEASURE PERFORMANCE
-  with torch.no_grad():
-      for rep in range(repetitions):
-          starter.record()
-          _ = model(dummy_input)
-          ender.record()
-          # WAIT FOR GPU SYNC
-          torch.cuda.synchronize()
-          curr_time = starter.elapsed_time(ender)
-          timings[rep] = curr_time
-  mean_syn = np.sum(timings) / repetitions
-  std_syn = np.std(timings)
-  mean_fps = 1000. / mean_syn
-  print(' * Mean@1 {mean_syn:.3f}ms Std@5 {std_syn:.3f}ms FPS@1 {mean_fps:.2f}'.format(mean_syn=mean_syn, std_syn=std_syn, mean_fps=mean_fps))
-  print(mean_syn)
+model.to(device)
+dummy_input = torch.randn(1, 3, 352, 352,dtype=torch.float).to(device)
+starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+repetitions = 1000
+timings=np.zeros((repetitions,1))
+#GPU-WARM-UP
+for _ in range(10):
+    _ = model(dummy_input)
+# MEASURE PERFORMANCE
+with torch.no_grad():
+    for rep in range(repetitions):
+        starter.record()
+        _ = model(dummy_input)
+        ender.record()
+        # WAIT FOR GPU SYNC
+        torch.cuda.synchronize()
+        curr_time = starter.elapsed_time(ender)
+        timings[rep] = curr_time
+mean_syn = np.sum(timings) / repetitions
+std_syn = np.std(timings)
+mean_fps = 1000. / mean_syn
+print(' * Mean@1 {mean_syn:.3f}ms Std@5 {std_syn:.3f}ms FPS@1 {mean_fps:.2f}'.format(mean_syn=mean_syn, std_syn=std_syn, mean_fps=mean_fps))
+print(mean_syn)
